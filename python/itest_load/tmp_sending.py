653e628b2ecbf8485d4591cbb02007ac6c2f262d
Stefano S <stefano@dianomic.com>
2017-08-18 15:54:42 +0200

    test

---
+++ python/itest_load/tmp_sending.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" test  """
+
+import sys
+
+# Template module loading
+module_template = "tmp_template_translator"
+__import__(module_template)
+plugin = sys.modules[module_template]
+
+
+def sending_test1():
+    """test """
+
+    print("Variable 5 - {0}".format(plugin.var))
+    plugin.plugin_init()
+
+
+def load(module_to_import):
+    """test """
+
+    global plugin
+
+    try:
+        __import__(module_to_import)
+
+    except ImportError:
+        print ("Unusable module {0}".format(module_to_import))
+        raise
+    else:
+        plugin = sys.modules[module_to_import]
+
+        print ("Sending OK")
+
+        print ("Variable 1 - {0}".format(plugin.var))
+        plugin.var = 20
+        print ("Variable 2 - {0}".format(plugin.var))
+
+        plugin.plugin_info()
+
+        print ("Variable 4 - {0}".format(plugin.var))
+
+
+if __name__ == "__main__":
+
+    print ("Test 1 ==============================================")
+
+    # load("tmp_omf_translator")
+    # sending_test1()
+    #
+    # print("Test 2 ==============================================")
+    # load("tmp_xxx_translator")
+    # sending_test1()
+    #
+    # try:
+    #     print("Test 3 ==============================================")
+    #     load("tmp_none")
+    #     sending_test1()
+    #
+    # except ImportError:
+    #     print("Exception ImportError")
---
+++ python/itest_load/tmp_xxx_translator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""test """
+
+var = 100
+
+
+def plugin_info():
+    """test """
+    print ("xxx translator info -  OK ")
+
+
+def plugin_init():
+    """test """
+    print ("xxx translator init -  OK ")
+
+
+if __name__ == "__main__":
+    print("xxx translator  plug in - MAIN")
